c04de84feebc6194adfab3a9c980305a0854e06d
dev-alamin1 <dev.alaminmondal@gmail.com>
2022-09-29 06:14:01 +0600

    Toastify added

--- src/component/ActivityButton/ActivityButton.js
+++ src/component/ActivityButton/ActivityButton.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './ActivityButton.css';
 
 const ActivityButton = () => {
+
+        const notify = () => toast("Congratulation For completing your Activity !");
+
     return (
         <div>
-            <button className='activity-button'>Activity Completed</button>
+            <button onClick={notify} className='activity-button'>Activity Completed</button>
+            <ToastContainer />
         </div>
     );
 };
